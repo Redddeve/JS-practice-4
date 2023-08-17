d0f8e92d3a6cb39cc86241c400c51d27fd14710f
Redddeve <popko.dmitry797@gmail.com>
2023-08-17 08:48:45 +0300

    Setting up the project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+
+/* 
+new SlimSelect({
+  select: '#selectElement',
+});
+
+Notify.failure('Qui timide rogat docet negare'); 
+*/
